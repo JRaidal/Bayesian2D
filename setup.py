67d130d56566e68075317f6f520ac6607604f8fe
JRaidal <juhantiitus.raidal@gmail.com>
2020-07-29 11:40:24 +0300

    4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Bayesian2D',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Package used to find the maximum or minimum of any 2D function using Bayesian optimization',
